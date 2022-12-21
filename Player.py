e74c3542f0f26e91522bd3f5c699e560a6498963
Mr.Zomar <85307356+Zomarnel@users.noreply.github.com>
2022-12-21 15:48:48 +0100

    Added Player Movement

--- Player.py
+++ Player.py
@@ -1,7 +1,9 @@
 class Player:
-    def __init__(self, name, health, gold, experience):
+    def __init__(self, name, health, gold, experience, x, y):
         self.Name = name
         self.Health = health
         self.Experience = experience
         self.Gold = gold
+        self.X = x
+        self.Y = y
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import Player
 
 
 def main():
@@ -6,12 +7,26 @@ def main():
 
     running = True
 
+    CurrentPlayer = Player.Player("XXX", 100, 50, 0, 400, 304)
+
     while running:
 
         for event in pygame.event.get():
+            # Quit event
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # Player Movement
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    CurrentPlayer.X += 16
+                if event.key == pygame.K_LEFT:
+                    CurrentPlayer.X -= 16
+                if event.key == pygame.K_DOWN:
+                    CurrentPlayer.Y += 16
+                if event.key == pygame.K_UP:
+                    CurrentPlayer.Y -= 16
 
 
 if __name__ == "main":
