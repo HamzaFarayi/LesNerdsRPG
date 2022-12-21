a631f05cc1ef240bd38c7eba3ce5bcf6ac3671e2
Mr.Zomar <85307356+Zomarnel@users.noreply.github.com>
2022-12-21 15:32:43 +0100

    Added main loop

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import pygame
+import pygame, sys
+
+
+def main():
+    pygame.init()
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == "main":
+    main()
