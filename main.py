11a996bf7c2005a590da38005c6c14320998eedb
HamzaFarayi <121093539+HamzaFarayi@users.noreply.github.com>
2022-12-21 13:01:40 +0100

    added screen

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-class Screen:
-  a = 1
+import pygame
+  
+background_colour = (234, 212, 252)
+  
+screen = pygame.display.set_mode((800, 608))
+  
+pygame.display.set_caption('Les Nerds RPG')
+  
+screen.fill(background_colour)
+  
+pygame.display.flip()
+  
+running = True
+  
+while running: 
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:
+            running = False
