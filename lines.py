ac89ba078d1f21c6bade85aea709e2e086a82899
KinY2 <121093511+KinY2@users.noreply.github.com>
2022-12-22 07:51:33 +0100

    Update lines.py

--- lines.py
+++ lines.py
@@ -13,9 +13,9 @@ class line:
       lineList.append(list_x)
 
       if a <= 48: #vetrical line
-        pygame.draw.line(screen, (255, 255, 255), (16*(a+1), 0), (16*(a+1), 608))
+        pygame.draw.line(screen, currentRegion.lineColor, (16*(a+1), 0), (16*(a+1), 608))
         
       if a <= 36: #horizontal line
-        pygame.draw.line(screen, (255, 255, 255), (0, 16*(a+1)), (800, 16*(a+1)))
+        pygame.draw.line(screen, currentRegion.lineColor, (0, 16*(a+1)), (800, 16*(a+1)))
 
       
