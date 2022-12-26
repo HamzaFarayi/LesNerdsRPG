815f8463bb968bccf6dba8ad3831ba7b1af08c91
HamzaFarayi <121093539+HamzaFarayi@users.noreply.github.com>
2022-12-26 22:06:35 +0100

    adjusted options screen :flower_playing_cards:

--- main.py
+++ main.py
@@ -144,11 +144,11 @@ def options():
 
         SCREEN.fill("white")
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        OPTIONS_TEXT = get_font(25).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(400, 104))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+        OPTIONS_BACK = Button(image=None, pos=(400, 308), 
                             text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
@@ -171,15 +171,15 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(50).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(30).render("MENU PRINCIPAL", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(425, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("images/Play Rect.png"), pos=(400, 240), 
-                            text_input="PLAY", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
+                            text_input="JOUER", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
         OPTIONS_BUTTON = Button(image=pygame.image.load("images/Options Rect.png"), pos=(400, 330), 
                             text_input="OPTIONS", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
         QUIT_BUTTON = Button(image=pygame.image.load("images/Quit Rect.png"), pos=(400, 420), 
-                            text_input="QUIT", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
+                            text_input="QUITTER", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
